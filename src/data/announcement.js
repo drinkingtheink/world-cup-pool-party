0459ba1f56f9e5415e40f3d7d798822770e868ac
Jason M Harrison <jason.m.harrison@gmail.com>
2026-07-19 12:44:04 -0500

    Update announcement.js

--- src/data/announcement.js
+++ src/data/announcement.js
@@ -8,7 +8,7 @@ export const announcement = {
   alwaysShow: false,
   id: '2026-07-19-infantino-thanks-v2',
   title: 'A Message from Gianni Infantino',
-  body: `On a mountain of group stage matches, in the stadium of your choosing, you sat on a throne of pool points. You watched. You suffered. You celebrated.\n\nDeath is but a doorway. Time is but a window. I will be back — and I will bring more teams.\n\nI, Gianni, Scourge of Zürich, Sorrow of Concacaf, say this to you now: you have made me feel like a child again. You have given this tournament meaning beyond the pitch.\n\nThank you. From the bottom of my very large heart.`,
+  body: `I, Gianni, Scourge of Zürich, Sorrow of Concacaf, say this to you now: you have made me feel like a child again. You have given this tournament meaning beyond the pitch.\n\nThank you. From the bottom of my very large heart.`,
   cta: 'Wrap. It. Up.',
   image: '/infantino-thanks.png',
 }
